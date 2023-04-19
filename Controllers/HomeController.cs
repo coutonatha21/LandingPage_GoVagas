b6e08cc6548acc6aa6caf63dd02f74dc8ceffd6c
Nathanael Couto <coutonathanael21@gmail.com>
2023-04-19 10:38:40 -0300

    first-commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMvcRazorCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.QuemSomos = "QUEM SOMOS";
+            ViewBag.Missao = "MISSÃO";
+            ViewBag.Visao = "VISÃO";
+            ViewBag.Valores = "VALORES";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contato";
+
+            return View();
+        }
+    }
+}
